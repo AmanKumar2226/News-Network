d5251e382ddd93b0593f752b428c3fb2aeff27ba
AmanKumar2226 <aman.k@infiscale.io>
2024-04-04 15:48:08 +0530

    NNI-task - Breadcrumb added

--- src/components/all-news.jsx
+++ src/components/all-news.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import NewsCard from "./news-card";
-import axios from "axios";
+
+
 
 export default function AllNews(){
     
@@ -8,6 +9,7 @@ export default function AllNews(){
     
     return(
         <>
+        
             <div className="px-10 py-10 flex flex-col gap-5 "> 
                 <h1>All News</h1>
                 <div className="card-container  ">
