c7a15e6d77535a6440ea339fcdb733f6b2b1d861
AmanKumar2226 <aman.k@infiscale.io>
2024-06-25 18:13:31 +0530

    NNI-redesign - About us page rerdesign

--- NNI-FE/src/components/business.jsx
+++ NNI-FE/src/components/business.jsx
@@ -6,7 +6,7 @@ import NewsCard from "./news-card";
 function Business() {
   return (
     <>
-      <div className="flex flex-col gap-5 flex-wrap justify-center mx-32 my-10">
+      <div className="flex flex-col gap-5 flex-wrap justify-center mx-40 my-10">
         <div className="flex gap-2">
           <img className="py-3" src="./images/red-icon.png" alt="" />
           <h1 className="text-3xl font-semibold text-black">Business</h1>
--- NNI-FE/src/components/science.jsx
+++ NNI-FE/src/components/science.jsx
@@ -7,7 +7,7 @@ function Science() {
 
   return (
     <>
-    <div className="flex flex-col gap-5  flex-wrap justify-center my-10 mx-32">
+    <div className="flex flex-col gap-5  flex-wrap justify-center my-10 mx-40">
       <div className="flex gap-2">
         <img className="py-3" src="./images/red-icon.png" alt="" />
         <h1 className="text-3xl font-semibold text-black">
