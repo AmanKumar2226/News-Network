cb52fbab3bcefae00f72c66ba46e547f5640d64f
AmanKumar2226 <aman.k@infiscale.io>
2024-07-15 15:26:06 +0530

    update news api created

--- NNI-BE/routes/newsRoutes.js
+++ NNI-BE/routes/newsRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addNews, deleteNews, getNews } from "../controllers/newsController.js";
+import { addNews, deleteNews, getNews, updateNews } from "../controllers/newsController.js";
 import upload from "../middlewares/multerConfig.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/add-news", upload.single("image"), addNews);
 router.get("/get-news", getNews);
 router.delete('/delete-news/:id',deleteNews);
+router.put('/update-news/:id',updateNews);
 
 export default router;
--- NNI-FE/src/components/carousel-with-one-card.jsx
+++ NNI-FE/src/components/carousel-with-one-card.jsx
@@ -1,144 +1,149 @@
-  import React from 'react'
-  import { useState, useEffect } from "react";
-  import '../component-styles/home.css'
-  import axios from "axios";
-  import Slider from "react-slick";
-  import "slick-carousel/slick/slick.css";
-  import "slick-carousel/slick/slick-theme.css"
-  import Spinner from './spinner';
+import React from 'react';
+import { useState, useEffect } from "react";
+import '../component-styles/home.css';
+import axios from "axios";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Spinner from './spinner';
 
-  function One() {
+function One() {
 
-    let [news, setNews] = useState([])
-    useEffect(() => {
-      let config = {
-        method: 'get',
-        maxBodyLength: Infinity,
-        url: 'http://localhost:8080/api/nni/get-news',
-        headers: {},
-        data: data
-      };
-
-      axios.request(config)
-        .then((response) => {
-          setNews(response.data)
-          
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-    }, [])
-    console.log(news)
-
-
-    const url = "https://saurav.tech/NewsAPI/everything/cnn.json";
-    // https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=01ef4ed41f254070bcf6f7ba22ad5d3c
-    let [data, setData] = useState([]);
-    let [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-      let config = {
-        method: "get",
-        maxBodyLength: Infinity,
-        url: url,
-        headers: {},
-      };
-
-      axios
-        .request(config)
-        .then((response) => {
-          setData(response.data.articles);
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }, []);
-
-    const settings = {
-      dots: true,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      initialSlide: 0,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            initialSlide: 1,
-            dots: false
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: false
-          }
-        }
-      ]
+  let [news, setNews] = useState([]);
+  useEffect(() => {
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:8080/api/nni/get-news',
+      headers: {},
     };
-    return (
-      <div className='flex gap-10 justify-center items-center min-[769px]:mx-44 max-[769px]:mx-2'>
-        <div className='flex w-1/2 gap-2 mb-4 max-[769px]:hidden  '>
-       <div className='h-[456px] rounded-xl flex gap-5 justify-center items-center w-full'>
-  {news.slice(-2).map((newsObj, index) => {
-    return (
-      <div className='relative w-1/2' key={index}>
-        <div className=''>
-          <a href={newsObj.url} target='_blank'>
-          <img className='h-[456px] w-[40vh] rounded-xl' src={`http://localhost:8080/${newsObj.image}`} alt="" />
-          </a>
-        </div>
-        <div className='p-4 rounded-xl absolute bg-[rgba(255,255,255,0.75)] dark:bg-[rgba(42,44,56,0.75)] dark:border-none border-[2px] w-[80%] right-[45px] bottom-5 flex flex-col gap-3'>
-          <h1 className='line-clamp-1 text-xl font-semibold'>{newsObj.title}</h1>
-          <p className='line-clamp-2'>{newsObj.description}</p>
+
+    axios.request(config)
+      .then((response) => {
+        setNews(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  }, []);
+  console.log(news);
+
+  const url = "https://saurav.tech/NewsAPI/everything/cnn.json";
+  let [data, setData] = useState([]);
+  let [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: url,
+      headers: {},
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        setData(response.data.articles);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+        }
+      }
+    ]
+  };
+
+  const isCompleteUrl = (url) => {
+    const regex = /^(http|https):\/\//;
+    return regex.test(url);
+  };
+
+  const sanitizeUrl = (url) => {
+    const cleanedUrl = url.replace(/[^\w-./:]/g, '');
+    if (cleanedUrl.includes('uploads') && !cleanedUrl.includes('uploads/')) {
+      return cleanedUrl.replace('uploads', 'uploads/');
+    }
+    return cleanedUrl;
+  };
+
+  return (
+    <div className='flex gap-10 justify-center items-center min-[769px]:mx-44 max-[769px]:mx-2'>
+      <div className='flex w-1/2 gap-2 mb-4 max-[769px]:hidden'>
+        <div className='h-[456px] rounded-xl flex gap-5 justify-center items-center w-full'>
+          {news.slice(-2).map((newsObj, index) => {
+            const imageUrl = newsObj.image
+              ? isCompleteUrl(newsObj.image)
+                ? sanitizeUrl(newsObj.image)
+                : `http://localhost:8080/${sanitizeUrl(newsObj.image)}`
+              : '';
+            return (
+              <div className='relative w-1/2 h-[50vh] rounded-xl' key={index} style={{background: `url(${imageUrl})`, backgroundSize: "cover", backgroundPosition:"center"}}>
+                <div className='p-4 rounded-xl absolute bg-[rgba(255,255,255,0.75)] dark:bg-[rgba(42,44,56,0.75)] dark:border-none border-[2px] w-[80%] right-[45px] bottom-5 flex flex-col gap-3'>
+                  <h1 className='line-clamp-1 text-xl font-semibold'>{newsObj.title}</h1>
+                  <p className='line-clamp-2'>{newsObj.description}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-    );
-  })}
-</div>
-        
-        </div>
-      
-        {isLoading && <Spinner />}
-        <div className="min-[769px]:w-1/2 mb-10 mt-5 ">
-          <div className="">
-            <Slider  {...settings}>
-              {data.map((dataObj, index) => {
-                return (
-                  <div className="w-[50%] h-full bg-white dark:bg-[#2A2C38] dark:text-white text-black rounded-xl">
-                    <div className="rounded-xl h-[50vh] flex justify-center items-end bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${dataObj.urlToImage})` }}>
-                      {/* <img className="h-[500px] w-full" src={dataObj.urlToImage} alt="image"/> */}
-                      <div className="flex flex-col bg-[rgba(255,255,255,0.75)] dark:bg-[rgba(42,44,56,0.75)]  dark:border-none gap-4  w-[95%] p-5 rounded-xl mb-6">
-                        <a href={dataObj.url} target='_blank'><p className="line-clamp-1 text-xl font-semibold">{dataObj.title}</p></a>
-                        <p className='line-clamp-2'>{dataObj.description}</p>
 
-                      </div>
-                    </div>
-
+      {isLoading && <Spinner />}
+      <div className="min-[769px]:w-1/2 mb-10 mt-5">
+        <div>
+          <Slider {...settings}>
+            {data.map((dataObj, index) => (
+              <div className="w-[50%] h-full bg-white dark:bg-[#2A2C38] dark:text-white text-black rounded-xl" key={index}>
+                <div className="rounded-xl h-[50vh] flex justify-center items-end bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${dataObj.urlToImage})` }}>
+                  <div className="flex flex-col bg-[rgba(255,255,255,0.75)] dark:bg-[rgba(42,44,56,0.75)] dark:border-none gap-4  w-[95%] p-5 rounded-xl mb-6">
+                    <a href={dataObj.url} target='_blank'>
+                      <p className="line-clamp-1 text-xl font-semibold">{dataObj.title}</p>
+                    </a>
+                    <p className='line-clamp-2'>{dataObj.description}</p>
                   </div>
-
-                )
-              })}
-            </Slider>
-          </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
+}
 
-  export default One
+export default One;
