1e67a8c4fb7b2a77452475e5412bc31d933fbca3
AmanKumar2226 <aman.k@infiscale.io>
2024-08-19 14:53:38 +0530

    images issue resolved

--- NNI-FE/src/Pages/HoroscopeDetails.jsx
+++ NNI-FE/src/Pages/HoroscopeDetails.jsx
@@ -26,19 +26,19 @@ function HoroscopeDetails() {
   }, [sign]);
 
   return (
-    <div className='my-20 mx-44 overflow-x-hidden dark:text-white'>
+    <div className='my-20 min-[769px]:mx-44 mx-2 overflow-x-hidden dark:text-white'>
       <section className='pb-44 border-b-4 flex justify-center items-center'>
         <div className='flex flex-col gap-2'>
-          <h1 className='text-[100px] font-semibold text-center uppercase'>Horoscope Detail</h1>
+          <h1 className='min-[769px]:text-[100px] text-[50px] font-semibold text-center uppercase'>Horoscope Detail</h1>
           <p className='text-center font-semibold'>Horoscope for {sign} sign</p>
         </div>
       </section>
       <section className='my-32'>
-        <div className='flex text-[54px] font-[700] gap-20 items-center justify-center'>
-          <img className='w-[450px] h-[513px]' src={theme === 'light' ? `/images/${sign.toLowerCase()}black.png` : `/images/${sign.toLowerCase()}.png`} alt={sign} />
-          <div className='w-1/2'>
-            <h1 className='text-[84px]'>{sign} <span className='text-[44px]'>{data.date}</span></h1>
-            <p className='text-[20px]'>{data.horoscope_data}</p>
+        <div className='flex text-[54px] font-[700] gap-20 items-center max-[769px]:flex-col justify-center'>
+          <img className='min-[769px]:w-[450px] min-[769px]:h-[513px]' src={theme === 'light' ? `/images/${sign.toLowerCase()}black.png` : `/images/${sign.toLowerCase()}.png`} alt={sign} />
+          <div className='min-[769px]:w-1/2'>
+            <h1 className='min-[769px]:text-[84px] text-[50px]'>{sign} <span className='min-[769px]:text-[44px] text-[18px]'>({data.date})</span></h1>
+            <p className='text-[20px] max-[769px]:font-normal text-justify'>{data.horoscope_data}</p>
           </div>
         </div>
       </section>
